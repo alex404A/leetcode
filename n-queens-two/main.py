9b9b692bc57363b81e95bc56a5458073f4782b32
alex404A <maxmalex1990@gmail.com>
2017-05-07 12:49:20 +0800

    update

---
+++ n-queens-two/main.py
@@ -0,0 +1,18 @@
+
+class Solution(object):
+    def solveNQueens(self, n):
+        def DFS(queens, xy_dif, xy_sum):
+            p = len(queens)
+            if p==n:
+                result.append(queens)
+                return None
+            for q in range(n):
+                if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
+                    DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])
+        result = []
+        DFS([],[],[])
+        return len(result)
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.solveNQueens(10))
