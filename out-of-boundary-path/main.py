e49fdcd850a94ac139f7f18689db125e9b5a27fe
alex404A <maxmalex1990@gmail.com>
2017-05-12 19:55:06 +0800

    update

--- out-of-boundary-path/main.py
+++ out-of-boundary-path/main.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def __init__(self):
         self.results = {}
+        self.divisor = pow(10, 9) + 7
 
     def findPaths(self, m, n, N, i, j):
         """
@@ -11,16 +12,12 @@ class Solution(object):
         :type j: int
         :rtype: int
         """
-        total = self.find(m, n, N, i, j)
-        return total % (pow(10, 9) + 7)
-
-    def find(self, m, n, N, i, j):
         if i == -1 or i == m or j == -1 or j == n:
             return 1
-        if N == 0:
+        if i >= N and m - i - 1 >= N and j >= N and n - j - 1 >= N:
             return 0
 
-        total = self.results.get((m, n, N, i, j))
+        total = self.results.get((N, i, j))
         if total is not None:
             return total
 
@@ -28,10 +25,10 @@ class Solution(object):
         rightCnt = self.findPaths(m, n, N - 1, i + 1, j)
         upCnt = self.findPaths(m, n, N - 1, i, j - 1)
         downCnt = self.findPaths(m, n, N - 1, i, j + 1)
-        total = leftCnt + rightCnt + upCnt + downCnt
-        self.results[(m, n, N, i, j)] = total
+        total = (leftCnt + rightCnt + upCnt + downCnt) % self.divisor
+        self.results[(N, i, j)] = total
         return total
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.findPaths(8, 50, 23, 5, 26))
+    print(solution.findPaths(8, 7, 16, 1, 5))
