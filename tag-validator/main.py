25e039e37b2f1ec7e3b14c63f86e1ae4643cfc15
alex404A <maxmalex1990@gmail.com>
2017-06-07 22:02:18 +0800

    update

--- tag-validator/main.py
+++ tag-validator/main.py
@@ -6,7 +6,6 @@ class Solution(object):
         self.endTagPattern = re.compile(r"</([A-Z]{1,9})>")
         self.startCdataPattern = re.compile(r"<\!\[CDATA\[")
         self.endCdataPattern = re.compile(r"\]\]>")
-        self.maxTagLen = 12
         self.tokens = []
 
     def isValid(self, code):
@@ -19,37 +18,48 @@ class Solution(object):
             return False
         i = 0
         while i < len(code):
-            if code[i] != '<':
-                i += 1
-                continue
-            startTagMatch = self.startTagPattern.match(code, i)
-            endTagMatch = self.endTagPattern.match(code, i)
-            startCdataMatch = self.startCdataMatch.match(code, i)
-            if startTagMatch is not None:
-                i = self.processStartTagMatch(startTagMatch)
-                continue
-            if endTagMatch is not None:
-                i, isValid = self.processEndTagMatch(endTagMatch)
-                if isValid:
+            if code[i] == '<':
+                startTagMatch = self.startTagPattern.match(code, i)
+                endTagMatch = self.endTagPattern.match(code, i)
+                startCdataMatch = self.startCdataPattern.match(code, i)
+                if startTagMatch is not None:
+                    i = self.processStartTagMatch(startTagMatch)
+                    continue
+                if endTagMatch is not None:
+                    i = self.processEndTagMatch(endTagMatch)
+                    if i == -1:
+                        return False
+                    if len(self.tokens) == 0 and i < len(code):
+                        return False
+                    continue
+                if startCdataMatch is not None:
+                    i = self.processStartCdataMatch(startCdataMatch)
+                    endCdataMatch = self.endCdataPattern.search(code, i)
+                    if endCdataMatch is None:
+                        return False
+                    i = self.processEndDataMatch(endCdataMatch)
                     continue
                 return False
-            if startCdataMatch is not None:
-                i = self.processStartCdataMatch(startCdataMatch)
-                endCdataMatch = self.endCdataPattern.match(code, i)
-                if endCdataPattern is None:
-                    return False
-                i = self.processEndDataMatch(endTagPattern)
-                continue
-        return True
+            i += 1
+        return True if len(self.tokens) == 0 else False
 
     def processStartTagMatch(self, match):
-        pass
+        self.tokens.append(match.group(1))
+        return match.end()
 
     def processEndTagMatch(self, match):
-        pass
+        endTagName = match.group(1)
+        if len(self.tokens) > 0 and endTagName == self.tokens.pop():
+            return match.end()
+        return -1
 
     def processStartCdataMatch(self, match):
-        pass
+        return match.end()
 
     def processEndDataMatch(self, match):
-        pass
+        return match.end()
+
+if __name__ == '__main__':
+    solution = Solution()
+    code = '<A>  </A><B></B>'
+    print(solution.isValid(code))
